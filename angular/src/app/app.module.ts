58be343325d45ab9d67e06bbdcd650b8facb31dc
Sorin <sorin.motoescu@gmail.com>
2022-11-16 20:09:20 +0200

    final changes

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 
@@ -10,7 +10,8 @@ import { TodosComponent } from './todos/todos.component';
     TodosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/src/app/todos/todos.component.ts
+++ angular/src/app/todos/todos.component.ts
@@ -10,6 +10,8 @@ export class TodosComponent implements OnInit {
 
   todos: Todo[] = [];
 
+    inputTodo:string ="";
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,9 +22,29 @@ export class TodosComponent implements OnInit {
       },
       {
         content: 'Second Todo',
-        completed: true
+        completed: false
       }
     ]
+  }
+  
+  toggleDone (id: number) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+      return v;
+    })
+    }
+
+  deleteTodo (id: number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }  
+
+  addTodo () {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+
+    this.inputTodo = "";
   }
 
 }
